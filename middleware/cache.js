cff6eb25a8c37835e4ef631e9db1bcad77f0e130
KUNDANYADAV7 <kdyd999@gmail.com>
2025-06-26 16:35:12 +0530

    cache

--- middleware/cache.js
+++ middleware/cache.js
@@ -1,3 +1,11 @@
+// import NodeCache from "node-cache";
+
+// export const cache = new NodeCache();
+
+
+
+
 import NodeCache from "node-cache";
 
-export const cache = new NodeCache();
+// 🟡 Set global default TTL of 60 seconds
+export const cache = new NodeCache({ stdTTL: 60 });
